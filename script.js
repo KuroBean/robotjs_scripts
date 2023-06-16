4412d7bd879a67bad1c844b1b1398599a3c803f8
Kensuke <kenshimojo@icloud.com>
2023-06-15 19:07:32 -0700

    with longtap command

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 const robot = require('robotjs');
 //when chat gpt window expanded right when width changes (to the wider one, when window abt half of screen)
 
-robot.setMouseDelay(100);
-var inputCoord = [1600, 1300];
-var sendBtn = [2300, 1300];
-var newchat = [1250, 150];
+robot.setMouseDelay(1000);
+var inputCoord = [1700, 1300];
+var sendBtn = [2350, 1300];
+var newchat = [1290, 150];
 var latestChat = [1277, 236];
 //USE TAMPERMONKEY SCRIPT SO CTRL KEY = SAVE LATEST RESPONSE, OTHER KEY = PASTE RESPONSE
-askChatGpt("shorter");
+setUpChat();
 makeNewChat();//NEED PARENTHESIS FOR COMMAND TO REGISTER IG
-askChatGpt("respond only within 3 lines shown in this website");
+askOtherChatResponse();
 goToNthChat(2);
+askOtherChatResponse();
+
+function setUpChat(){
+    
+    
+    keyLongTap("control");
+    robot.moveMouse(inputCoord[0], inputCoord[1]);
+    robot.mouseClick("right");
+
+    keyLongTap("space");
+    keyLongTap("backspace");
+    robot.moveMouse(sendBtn[0], sendBtn[1]);
+    robot.mouseToggle("down","right");
+    //CLICK DOESNT WORK WITH SEND BUTTON, BUT
+    //IF YOU DO THIS AND HOLD IT DOWN LONGER IT SOMEHOW WORKS
+    pause(1000);
+    robot.mouseToggle("up", "right");
+    console.log("clicked");
+    pause(5000);
+    keyLongTap("shift");
+}
+
+function askOtherChatResponse(){
+    keyLongTap("alt");
+    robot.moveMouse(inputCoord[0], inputCoord[1]);
+    robot.mouseClick("right");
+    keyLongTap("space");
+    keyLongTap("backspace");
+    robot.moveMouse(sendBtn[0], sendBtn[1]);
+    robot.mouseToggle("down","right");
+    //CLICK DOESNT WORK WITH SEND BUTTON, BUT
+    //IF YOU DO THIS AND HOLD IT DOWN LONGER IT SOMEHOW WORKS
+    pause(1000);
+    robot.mouseToggle("up", "right");
+    console.log("clicked");
+    pause(5000);
+    keyLongTap("shift");
+}
+
+function keyLongTap(key){
+    robot.keyToggle(key,"down");
+    pause(500);
+    robot.keyToggle(key,"up");
+}
 
 function pause(millis) {
     var date = Date.now();
@@ -19,7 +63,7 @@ function pause(millis) {
         curDate = Date.now();
     } while (curDate-date < millis);
 }
- function askChatGpt(question) {
+function askChatGpt(question) {
     robot.moveMouse(inputCoord[0], inputCoord[1]);
     robot.mouseClick("right");
     robot.typeString(question );
